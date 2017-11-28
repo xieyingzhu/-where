4ab890e9b403b2ec8114b386b9d253840106e1b1
xieyingzhu <116995651@qq.com>
2017-11-28 23:02:14 +0800

    练习

---
+++ 2017-11-28.py
@@ -0,0 +1,91 @@
+"""
+练习习题-谢莹珠
+
+"""
+date:2017-11-28
+
+"""
+#算数运算符
+a=21
+b=10
+c=0
+c=a+b
+print("line 1 - value of c is",c)
+
+c=a-b
+print("line 2 - value of c is",c)
+
+c=a*b
+print("line 3 - value of c is",c)
+
+c=a/b
+print("line 4 - value of c is",c)
+
+c=a%b
+print("line 5 - value of c is",c)
+
+a=2
+b=3
+c=a**b
+print("line 6 - value of c is",c)
+
+c=a//b
+print("line 7 - value of c is",c)
+
+#比较运算符
+a=21
+b=10
+c=0
+if ( a == b):
+    print(line 1 - a is equal to b)
+    else:  
+        print(line 1 - a is not equal to b)
+
+if ( a != b):
+     print(line 2 - a is not equal to b)       
+     else：
+      print(line 2 - a is  equal to b) 
+ if ( a <> b):
+    print(line 3 - a is not equal to b)  
+    else:
+     print(line 3 - a is  equal to b)    
+if( a < b ):
+    print(line 4 - a is less than b)        
+    else:
+     print(line 4 - a is not less than b)     
+if (a > b):
+    print(line 5 - a is great than b)     
+    else:
+    print(line 5 - a is not great than b)  
+
+a=5
+b=20
+if(a <= b):
+    print(line 6 - a is either less than or equal to b)   
+    else:
+        print(line 6 - a is neither less than or equal to b)    
+if(a >= b):
+    print(line 7 - a is either great than or equal to b)  
+    else:
+     print(line 7 - a is neither great than or equal to b) 
+
+#赋值运算符
+a=21
+b=10
+c=0
+c= a + b
+print(line 1 - value of c is, c)
+c += a
+print(line 2 - value of c is, c)
+c *= a
+print(line 3 - value of c is, c)
+c /= a
+print(line 4 - value of c is, c)
+c = 2
+c %= a
+print(line 5 - value of c is, c)
+c **= a
+print(line 6 - value of c is, c)
+c //= a
+print(line 7 - value of c is, c)
+
